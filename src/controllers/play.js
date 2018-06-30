2522e74586579f3514d3c1086cc42786d5c0435c
Matthew Roelle <mroelle@renovateamerica.com>
2018-06-30 12:57:33 -0700

    Getting there!

--- src/controllers/play.js
+++ src/controllers/play.js
@@ -1,15 +1,22 @@
 const FOOD_TYPES = {
-	CARROT: { spr: "carrot", },
-	EGG: { spr: "egg", },
-	MUSHROOM: { spr: "mushroom" },
-	ONION: { spr: "onion" },
-	PEPPER: { spr: "pepper" },
-	POTATO: { spr: "potato" },
-	NOODLES: { spr: "noodles" },
-	TOMATO: { spr: "tomato" },
+	CARROT: { spr: "carrot", id: 1, },
+	EGG: { spr: "egg", id: 2 },
+	MUSHROOM: { spr: "mushroom", id: 3 },
+	ONION: { spr: "onion", id: 4 },
+	PEPPER: { spr: "pepper", id: 5 },
+	POTATO: { spr: "potato", id: 6 },
+	NOODLES: { spr: "noodles", id: 7 },
+	TOMATO: { spr: "tomato", id: 8 },
 };
 
 const FOOD = [ FOOD_TYPES.CARROT, FOOD_TYPES.EGG, FOOD_TYPES.MUSHROOM, FOOD_TYPES.ONION, FOOD_TYPES.PEPPER, FOOD_TYPES.POTATO, FOOD_TYPES.NOODLES, FOOD_TYPES.TOMATO ];
+
+const PLAY_STATES = {
+	IN_PLANE: 0,
+	FALLING: 1,
+	LANDING: 2,
+	SCORING: 3
+};
 
 class PlayController {
 	constructor(params) {
@@ -21,15 +28,45 @@ class PlayController {
 		this.uiFrame = game.phaser.add.sprite(0, 0, "frame");
 		this.uiPot = game.phaser.add.sprite(18, 420, "pot");
 		this.uiLid = game.phaser.add.sprite(18, 390, "lid");
+
+		this.altMeter = game.phaser.add.sprite(61, 80, "alt-meter");
+		this.altMarker = game.phaser.add.sprite(81, 58, "alt-marker");
+		this.altitude = 12000;
+		this.startTime = game.phaser.time.now;
+
 		this.uiGroup.addChild(this.uiFrame);
 		this.uiGroup.addChild(this.uiPot);
 		this.uiGroup.addChild(this.uiLid);
+		this.uiGroup.addChild(this.altMeter);
+		this.uiGroup.addChild(this.altMarker);
 
 		this.background = game.phaser.add.tileSprite(162, 16, 618, 564, "background");
 		this.gameGroup.addChild(this.background);
 
 		this.player = new Player(this.gameGroup);
+		this.player.sprite.y = -100;
 		this.food = [];
+
+		this.lastCloudSpawn = -1000;
+		this.clouds = [];
+		for(let i = 0; i < 50; i++) this.spawnCloud(true);
+		this.foodExplosion = [];
+		this.planeSprite = game.phaser.add.sprite(0, 100, "plane");
+		this.planeSprite.pivot.set(0.5);
+		this.planeSprite.anchor.set(0.5);
+
+		this.planeCentered = false;
+		game.phaser.add.tween(this.planeSprite)
+		.to({ x: 480,  },
+			3500, "Quad.easeOut", true, 0)
+		.onComplete.add(() => {
+			const _this = this;
+			setTimeout(() => {
+				_this.planeCentered = true;
+			}, 700);
+		}, this);
+
+		this.gameGroup.addChild(this.planeSprite);
 
 		this.lastFoodSpawn = -1000;
 
@@ -38,6 +75,17 @@ class PlayController {
 		this.lidTween = null;
 
 		this.destroyables.push(this.uiGroup);
+
+		this.state = PLAY_STATES.IN_PLANE;
+
+		this.altText = game.phaser.add.text(24, 30, "12000 ft", {
+			font: "18px slkscr",
+			fill: "#ffffff",
+			stroke: "#000000",
+			strokeThickness: 4,
+			align: "center"
+		});
+		this.uiGroup.addChild(this.altText);
 	}
 
 	openLid() {
@@ -46,7 +94,7 @@ class PlayController {
 		const _this = this;
 		setTimeout(() => {
 			_this.closeLid();
-		}, 1100);
+		}, 550);
 
 		if (this.lidOpen) return;
 		this.lidOpen = true;
@@ -54,12 +102,11 @@ class PlayController {
 		if (!!this.lidTween) this.lidTween.stop();
 
 		this.lidTween = game.phaser.add.tween(this.uiLid)
-		.to({ x: 18, y: 300, angle: -20 },
+		.to({ x: -50, y: 330, angle: -20 },
 			500, "Quad.easeOut", true, 0);
 	}
 	
 	closeLid() {
-		console.log("c");
 		if (!this.lidOpen) return;
 		if (game.phaser.time.now - this.lastOpen < 500) return;
 
@@ -76,16 +123,134 @@ class PlayController {
 	}
 
 	update() {
+		switch(this.state) {
+			case PLAY_STATES.IN_PLANE:
+				this.planeUpdate();
+				break;
+			case PLAY_STATES.FALLING:
+				this.fallUpdate();
+				break;
+			case PLAY_STATES.LANDING:
+				this.landUpdate();
+				break;
+			case PLAY_STATES.SCORING:
+				this.scoringUpdate();
+				break;
+		}
+
+		game.phaser.world.bringToTop(this.uiGroup);
+	}
+
+	scoringUpdate() {
+
+	}
+
+	landUpdate() {
+		const t = game.phaser.time.now;
+		const dt = t - this.startedLanding;
+
+		const e = Math.max(0, (1 - (dt/2000)));
+
+		this.background.tilePosition.y -= 3 * e;
+
+		this.gameGroup.bringToTop(this.player.sprite);
+	}
+	
+	spawnCloud(randx) {
+		let newCloud;
+
+		if (randx) {
+			newCloud = game.phaser.add.sprite(100 + (Math.random() * 700), 5 + (Math.random()*80), "cloud1");
+		} else {
+			newCloud = game.phaser.add.sprite(900, 5 + (Math.random()*80), "cloud1");
+		}
+
+		this.clouds.push(newCloud);
+		this.gameGroup.addChild(newCloud);
+		newCloud.speed = 2 + (Math.random()*3);
+	}
+
+	planeUpdate() {
 		const t = game.phaser.time.now;
 
-		if (t - this.lastFoodSpawn > 300) {
+		if (t - this.lastCloudSpawn > 100) {
+			this.lastCloudSpawn = t;
+			this.spawnCloud();
+		}
+
+		for(let c of this.clouds) {
+			c.x -= c.speed;
+		}
+
+		if (this.planeCentered) {
+			this.state = PLAY_STATES.FALLING;
+
+			game.phaser.camera.shake(0.5, 500);
+
+			this.startTime = game.phaser.time.now;
+			this.player.sprite.x = 480;
+			this.player.sprite.y = 100;
+			this.player.velocity.y = 6;
+
+			for(let i = 0; i < 60; i++) {
+				const f = FOOD[Math.floor(Math.random() * FOOD.length)];
+				const spr = game.phaser.add.sprite(480, 90, f.spr);
+				spr.velocity = {
+					x: (Math.random() * 30) - 15,
+					y: (7 + (Math.random() * 9))
+				};
+				spr.pivot.set(0.5);
+				spr.anchor.set(0.5);
+				this.gameGroup.addChild(spr);
+				this.foodExplosion.push(spr);
+			}
+
+			const _this = this;
+			setTimeout(() => {
+				for(let c of _this.clouds) {
+					c.destroy();
+				}
+				for(let f of _this.foodExplosion) {
+					f.destroy();
+				}
+				_this.clouds = [];
+				_this.foodExplosion = [];
+			}, 3000);
+		}
+
+		this.planeSprite.y = 100 + (Math.sin(t/300)*20);
+
+		this.gameGroup.bringToTop(this.planeSprite);
+	}
+
+	fallUpdate() {
+		const t = game.phaser.time.now;
+		const dt = t - this.startTime;
+
+		this.altText.text = this.altitude + " ft";
+		this.altitude = Math.floor(12000 - (dt*0.3));
+		this.altMarker.y = 58 + ((1 - (this.altitude/12000))*265);
+
+		for(let f of this.foodExplosion) {
+			f.x += f.velocity.x;
+			f.y += f.velocity.y;
+			f.angle += 8;
+		}
+
+		for(let c of this.clouds) {
+			c.x -= c.speed;
+			c.y -= c.speed*0.8;
+		}
+
+		this.planeSprite.y -= 3;
+
+		if (t - this.lastFoodSpawn > 120) {
 			this.lastFoodSpawn = t;
 			this.spawnFood();
 		}
 
 		this.background.tilePosition.y -= 3;
 		this.player.update();
-		game.phaser.world.bringToTop(this.uiGroup);
 
 		for(let f of this.food) {
 			if (game.utils.dist(this.player.sprite.x, this.player.sprite.y, f.sprite.x, f.sprite.y) < 35) {
@@ -102,6 +267,71 @@ class PlayController {
 		}
 
 		this.food = this.food.filter(f => !f.dead);
+
+		if (this.altitude < 0) {
+			this.state = PLAY_STATES.LANDING;
+			this.altText.text = "0 ft";
+			this.startedLanding = game.phaser.time.now;
+			this.player.sprite.loadTexture("chef-parachute");
+			this.player.sprite.y -= 50;
+			this.player.emitter1.destroy();
+			this.player.emitter2.destroy();
+
+			game.phaser.add.tween(this.player.sprite)
+			.to({ x: 450, y: 450, angle: 0 },
+				3000, "Quad.easeOut", true, 0)
+
+			.onComplete.add(() => {
+				this.state = PLAY_STATES.SCORING;
+
+				const nTypes = {};
+				let j = 0;
+				for(let f of this.food) {
+					if (f.pickedUp) { 
+						if (!nTypes[f.typ.id]) nTypes[f.typ.id] = { ord: j++, n: 1 };
+						else nTypes[f.typ.id].n++;
+
+						const countInfo = nTypes[f.typ.id];
+
+						const ntyp = countInfo.n;
+
+						const targetY = 50 + (countInfo.ord * 40);
+						const targetX = 300 + (ntyp * 10);
+
+						this.uiGroup.bringToTop(f.sprite);
+						game.phaser.add.tween(f.sprite)
+						.to({ x: targetX, y: targetY, angle: 720 },
+							1000, "Quad.easeOut", true, 0)
+					}
+				}
+
+				for(let k in nTypes) {
+					const countInfo = nTypes[k];
+					const txt = game.phaser.add.text(240, 32 + (countInfo.ord*40), countInfo.n.toString(), {
+						font: "26px slkscr",
+						fill: "#ffffff",
+						stroke: "#000000",
+						strokeThickness: 4,
+						align: "left"
+					});
+				}
+			}, this);
+
+
+			this.groundSprite = game.phaser.add.sprite(124, 600, "ground1");
+			this.gameGroup.addChild(this.groundSprite);
+
+			game.phaser.add.tween(this.groundSprite)
+			.to({ y: 430 },
+				2000, Phaser.Easing.Linear.None, true, 0);
+
+			for(let f of this.food) {
+				this.gameGroup.bringToTop(f.sprite);
+				game.phaser.add.tween(f.sprite)
+				.to({ y: 440 + (Math.random() * 30), angle: 0 },
+					2000, Phaser.Easing.Linear.None, true, 0);
+			}
+		}
 	}
 
 	render() {
@@ -208,6 +438,7 @@ class Food {
 
 		if (this.rotationSpeed < 1 && this.rotationspeed > -1) this.rotationSpeed = 1;
 
+		this.typ = typ;
 		this.grp = grp;
 		this.uigrp = uigrp;
 	}
@@ -222,14 +453,14 @@ class Food {
 
 		const t = game.phaser.add.tween(this.sprite)
 		.to({ x: 50 + (Math.random()*60), y: 430 + Math.random()*20, angle: this.sprite.angle + 720 },
-			500, "Quad.easeOut", true, 0);
+			750, "Quad.easeOut", true, 0);
 
 		t.onComplete.add(() => {
 
 		}, this);
 	}
 
-	update() {
+	update(landing) {
 		if (this.pickedUp) {
 		} else {
 			this.sprite.position.y += this.speed;
--- src/loader.js
+++ src/loader.js
@@ -4,6 +4,7 @@ class Loader {
 		g.load.image("pot", "assets/export-pot.png");
 		g.load.image("background", "assets/export-background.png");
 		g.load.image("chef-falling", "assets/export-chef-falling.png");
+		g.load.image("chef-parachute", "assets/export-chef-parachute.png");
 		g.load.image("particle1", "assets/export-particle1.png");
 		g.load.image("particle2", "assets/export-particle2.png");
 		g.load.image("particle3", "assets/export-particle3.png");
@@ -19,6 +20,14 @@ class Loader {
 		g.load.image("tomato", "assets/export-tomato.png");
 
 		g.load.image("lid", "assets/export-lid.png");
+
+		g.load.image("alt-meter", "assets/export-altitude-meter.png");
+		g.load.image("alt-marker", "assets/export-chef-alt.png");
+
+		g.load.image("plane", "assets/export-plane.png");
+		g.load.image("cloud1", "assets/export-cloud1.png");
+
+		g.load.image("ground1", "assets/export-ground1.png");
 	}
 }
 
